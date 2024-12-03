d8c9bff8e83fa85ad1a0c3f66af57a27622fc0f1
Andrew John Buggee <84047898+andrewjbuggee@users.noreply.github.com>
2024-12-02 17:04:18 -0700

    hyperspectral retrieval

--- Hyperspectral_Cloud_Retrievals/EMIT/write_INP_file_4EMIT_homogenous.m
+++ Hyperspectral_Cloud_Retrievals/EMIT/write_INP_file_4EMIT_homogenous.m
@@ -11,27 +11,6 @@ function [inpNames, inputs] = write_INP_file_4EMIT_homogenous(inputs, pixels2use
 % ------------------------------------------------
 
 
-% what computer are we using?
-
-% a template file has been set up to be edited and saved as a new file
-% determine which computer is being used
-userName = whatComputer;
-
-if strcmp(userName,'anbu8374')
-
-    libRadTran_path = ['/Users/anbu8374/Documents/LibRadTran/libRadtran-2.0.4'];
-
-elseif strcmp(userName,'andrewbuggee')
-
-    libRadTran_path = ['/Users/andrewbuggee/Documents/CU-Boulder-ATOC/Hyperspectral-Cloud-Droplet-Retrieval/',...
-        'LibRadTran/libRadtran-2.0.4'];
-
-else
-    error('I dont recognize this computer user name')
-end
-
-
-addpath(libRadTran_path);
 
 %% DEFINE UVSPEC INP FILE INPUTS
 
@@ -122,7 +101,7 @@ tau_vec = tau_vec(:)';
 
 % For now, lets only use the midpoint of the EMIT band in the visible
 % spectrum used for this analysis
-wavelength_forTau = emit.radiance.wavelength(inputs.bands2run(1));                % nm
+wavelength_forTau = 500;                % nm
 
 
 
@@ -375,21 +354,6 @@ for pp = 1:length(pixel_row)
                 fprintf(fileID, formatSpec,'day_of_year', inputs.RT.day_of_year, ' ', '# accounts for changing Earth-Sun distance');
 
 
-                % Define the total precipitable water
-                % -------------------------------------------------------------------------
-                % Define override of total precipitable water. This will force the total
-                % column of water vapor to be whatever value you define.
-                % If you don't wish to change the default, define the variable with nan
-
-                if inputs.RT.use_MODIS_aboveCloudWaterVapor==true
-
-                    total_h2O_column = inputs.RT.effectiveWaterThickness.vapor(pp) * 10;    % mm of precipitable water
-
-                    if isnan(total_h2O_column)==false
-                        formatSpec = '%s %s %f %s %5s %s \n';
-                        fprintf(fileID, formatSpec,'mol_modify','H2O', total_h2O_column,' MM', ' ', '# Total Precipitable Water');
-                    end
-                end
 
 
                 % Define the surface albedo
@@ -407,19 +371,6 @@ for pp = 1:length(pixel_row)
                     formatSpec = '%s %s %5s %s \n';
                     fprintf(fileID, formatSpec,'wc_file 1D', ['../data/wc/',wc_filename{rr,tt}], ' ', '# Location of water cloud file');
                     %fprintf(fileID, formatSpec,'wc_file 1D', ['../data/wc/',wc_filename{1}(1)], ' ', '# Location of water cloud file');
-
-                    % Define the percentage of horizontal cloud cover
-                    % This is a number between 0 and 1
-                    % ------------------------------------------------
-                    if inputs.RT.linear_cloudFraction==true
-                        %percent_cloud_cover(nn) = -0.1082 * modis.EV1km.reflectance(row(nn), col(nn),1) + 0.92164;      % custom linear fit 1 (seventh pass figure)
-                        cloud_cover = (0.89 - 0.8459)/(0.2 - 0.7) * emit.reflectance(inputs.bands2use(1),pp) +...
-                            (0.8459 - 0.7*(0.89 - 0.8459)/(0.2 - 0.7));      % custom linear fit 2 (Ninth pass figure)
-                    else
-                        cloud_cover = inputs.RT.cloud_cover;
-                    end
-                    formatSpec = '%s %f %5s %s \n';
-                    fprintf(fileID, formatSpec,'cloudcover wc', cloud_cover, ' ', '# Cloud cover percentage');
 
 
                     % Define the technique or parameterization used to convert liquid cloud
@@ -440,19 +391,33 @@ for pp = 1:length(pixel_row)
                     wavelengths(bb,end), ' ', '# Wavelength range');
 
 
-                % Define the column water vapor amount
-                % --------------------------------------------------------------------
-                if inputs.RT.modify_waterVapor==true
 
-                    % If true, modify the amount of column water vapor
-                    % --------------------------------------------------------------
-                    formatSpec = '%s %f %s %5s %s \n\n';
-                    fprintf(fileID, formatSpec,'mol_modify H2O ', inputs.RT.waterVapor_column, ' MM', ' ', '# Column water vapor amount');
+                % Define the total precipitable water
+                % -------------------------------------------------------------------------
+                % Define override of total precipitable water. This will force the total
+                % column of water vapor to be whatever value you define.
+                % If you don't wish to change the default, define the variable with nan
+
+                if inputs.RT.use_MODIS_aboveCloudWaterVapor==true
+
+                    total_h2O_column = inputs.RT.effectiveWaterThickness.vapor(pp) * 10;    % mm of precipitable water
+
+
+                elseif inputs.RT.modify_waterVapor==true
+
+                    total_h2O_column = inputs.RT.waterVapor_column;     % mm of precipitable water
 
 
                 end
 
+                % tell libRadtran the total column water vapor amount
+                if isnan(total_h2O_column)==false
+                    formatSpec = '%s %s %f %s %5s %s \n';
+                    fprintf(fileID, formatSpec,'mol_modify','H2O', total_h2O_column,' MM', ' ', '# Total Precipitable Water');
+                end
 
+
+                
 
                 % Define the concentration of carbon dioxide
                 % --------------------------------------------------------------------
