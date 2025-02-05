8886ba571f6976c7babf47b5706059e9dde2be66
Andrew John Buggee <84047898+andrewjbuggee@users.noreply.github.com>
2025-02-05 12:36:42 -0700

    metadata read code

---
+++ Hyperspectral_Cloud_Retrievals/MODIS_Cloud_Retrieval/test_code.m
@@ -0,0 +1,14 @@
+
+
+L1B_metadata = hdfread(fileName, 'Level 1B Swath Metadata');
+
+raw_time = L1B_metadata{5};     % TAI time (number of sec. since 1/1/93)
+
+% convert this to UTC time
+% MODIS EV Sector time starts on 1 Jan 1993
+epoch = datetime(1993,1,1,'TimeZone','UTCLeapSeconds');
+
+% now lets add secods according to the EV sector start time measurements
+modis_pixel_time = epoch + seconds(raw_time);
+
+
