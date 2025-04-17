537471656f262a22d561fd36d4d730121bd09d9c
Andrew John Buggee <anbu8374@colorado.edu>
2025-04-16 18:10:33 -0600

    minor updates to retrieval of MODIS data and new figures

--- Hyperspectral_Cloud_Retrievals/MODIS_Cloud_Retrieval/retrieveMODIS_data.m
+++ Hyperspectral_Cloud_Retrievals/MODIS_Cloud_Retrieval/retrieveMODIS_data.m
@@ -41,69 +41,69 @@ for ii = 1:length(files)
             % 250m resolution calibrated data
 
             L1B_fileNames{ii} = file_ii;
-            modis.EV250m = compute_MODIS_sub_pixel_heterogeneity(file_ii);
+            %modis.EV250m = compute_MODIS_sub_pixel_heterogeneity(file_ii);
 
 
-            if strcmp(file_ii(1:8),'MOD02HKM') == true || strcmp(file_ii(1:8),'MYD02HKM') == true
-                % 500m resolution calibrated data
+        elseif strcmp(file_ii(1:8),'MOD02HKM') == true || strcmp(file_ii(1:8),'MYD02HKM') == true
+            % 500m resolution calibrated data
 
-                L1B_fileNames{ii} = file_ii;
-                modis.EV500m = readMODIS_L1B_data(file_ii);
-
-            elseif strcmp(file_ii(1:8),'MOD021KM') == true || strcmp(file_ii(1:8),'MYD021KM') == true
-                % 1km resolution data, which is the same resolution as the
-                % cloud microphysical retrievals
-
-                L1B_fileNames{ii} = file_ii;
-                modis.EV1km = readMODIS_L1B_data(file_ii);
-
-            end
-
-
-
-        elseif strcmp(file_ii(1:5),'MOD03') == true || strcmp(file_ii(1:5), 'MYD03') == true
-
-            % save the filename
             L1B_fileNames{ii} = file_ii;
+            modis.EV500m = readMODIS_L1B_data(file_ii);
 
-            % extract geolocation data from hdf files
-            [modis.sensor,modis.solar,modis.geo] = readMODIS_geolocation(file_ii);
+        elseif strcmp(file_ii(1:8),'MOD021KM') == true || strcmp(file_ii(1:8),'MYD021KM') == true
+            % 1km resolution data, which is the same resolution as the
+            % cloud microphysical retrievals
 
-        elseif strcmp(file_ii(1:5),'MOD06') == true || strcmp(file_ii(1:5), 'MYD06') == true
-
-            % save the filename
             L1B_fileNames{ii} = file_ii;
-
-            % Retrive the true MODIS cloud Properties
-
-            modis.cloud = readMODIS_L2_data(file_ii);
-
-
-        elseif strcmp(file_ii(1:5), 'MOD05') == true || strcmp(file_ii(1:5), 'MYD05') == true
-
-            % save the filename
-            L1B_fileNames{ii} = file_ii;
-
-            % Retrieve the water vapor column estimates
-            modis.vapor = readMODIS_L2_waterVapor_data(file_ii);
-
-        elseif strcmp(file_ii(1:5),'MOD35') == true || strcmp(file_ii(1:5), 'MYD35') == true
-
-            % This is the cloud mask file that contains the computation of H,
-            % the horizontal homogeneity index
-
-            % save the filename
-            L1B_fileNames{ii} = file_ii;
-
-            modis.cloud.inhomogeneity_index = read_inhomogeneity_index(file_ii);    % percent
-
-
+            modis.EV1km = readMODIS_L1B_data(file_ii);
 
         end
 
 
 
+    elseif strcmp(file_ii(1:5),'MOD03') == true || strcmp(file_ii(1:5), 'MYD03') == true
+
+        % save the filename
+        L1B_fileNames{ii} = file_ii;
+
+        % extract geolocation data from hdf files
+        [modis.sensor,modis.solar,modis.geo] = readMODIS_geolocation(file_ii);
+
+    elseif strcmp(file_ii(1:5),'MOD06') == true || strcmp(file_ii(1:5), 'MYD06') == true
+
+        % save the filename
+        L1B_fileNames{ii} = file_ii;
+
+        % Retrive the true MODIS cloud Properties
+
+        modis.cloud = readMODIS_L2_data(file_ii);
+
+
+    elseif strcmp(file_ii(1:5), 'MOD05') == true || strcmp(file_ii(1:5), 'MYD05') == true
+
+        % save the filename
+        L1B_fileNames{ii} = file_ii;
+
+        % Retrieve the water vapor column estimates
+        modis.vapor = readMODIS_L2_waterVapor_data(file_ii);
+
+    elseif strcmp(file_ii(1:5),'MOD35') == true || strcmp(file_ii(1:5), 'MYD35') == true
+
+        % This is the cloud mask file that contains the computation of H,
+        % the horizontal homogeneity index
+
+        % save the filename
+        L1B_fileNames{ii} = file_ii;
+
+        modis.cloud.inhomogeneity_index = read_inhomogeneity_index(file_ii);    % percent
+
+
+
     end
+
+
+
+end
 
 
 
--- Presentations_and_Papers/first_paper/first_paper_figures.m
+++ Presentations_and_Papers/first_paper/first_paper_figures.m
@@ -5414,7 +5414,7 @@ end
 modisData = {'2008_11_09/', '2008_11_11_1430/', '2008_11_11_1850/'};
 
 % variables to keep
-effRad_uncert_17 = [];
+H_sp = [];
 optThickness_uncert_17 = [];
 
 effRad_uncert_17_percent = [];
@@ -5453,7 +5453,7 @@ for nn = 1:length(modisData)
     temp_effRad_uncert_17(pix_lessThan0_or_Nan_or_tauLessThan) = [];
 
     % store remaining values in a global variable
-    effRad_uncert_17 = [effRad_uncert_17; temp_effRad_uncert_17];        % microns
+    H_sp = [H_sp; temp_effRad_uncert_17];        % microns
 
 
     % grab effective radius uncertainty over ocean 
@@ -5512,7 +5512,7 @@ end
 figure; 
 
 subplot(1,2,1)
-histogram(effRad_uncert_17)
+histogram(H_sp)
 grid on; grid minor
 ylabel('Counts', 'interpreter', 'latex')
 xlabel('$\delta r_e$ $(\mu m)$', 'interpreter', 'latex')
@@ -5556,8 +5556,8 @@ disp([newline, 'Average COD uncertainty for pixels over ocean: ', ...
 
 % print the mean values of uncertainty for each variable
 disp([newline, 'Average CER uncertainty for pixels over ocean: ', ...
-    num2str(mean(effRad_uncert_17)), ' microns, with a standard deviation of: ',...
-    num2str(std(effRad_uncert_17)), ' microns', newline])
+    num2str(mean(H_sp)), ' microns, with a standard deviation of: ',...
+    num2str(std(H_sp)), ' microns', newline])
 disp([newline, 'Average COD uncertainty for pixels over ocean: ', ...
     num2str(mean(optThickness_uncert_17)), ', with a standard deviation of: ',...
     num2str(std(optThickness_uncert_17)), newline])
@@ -5656,4 +5656,218 @@ end
 
 t = Tiff('MYD021KM.A2008316.1850.061.2018039033053.hdf_reprojected.tif', 'r');
 imageData = read(t);
+
+
+
+%% For Zhibo Zhang - Histogram of sub-pixel inhomogeneity for all cloud pixels over ocean
+
+clear variables
+
+
+% Determine which computer you're using
+
+% Find the folder where the mie calculations are stored
+% find the folder where the water cloud files are stored.
+if strcmp(whatComputer,'anbu8374')==true
+
+    % -----------------------------------------
+    % ------ Folders on my Mac Desktop --------
+    % -----------------------------------------
+
+    % ***** Define the MODIS Folder *****
+
+    modisFolder = ['/Users/anbu8374/Documents/MATLAB/Matlab-Research/Hyperspectral_Cloud_Retrievals/',...
+        'MODIS_Cloud_Retrieval/MODIS_data/'];
+
+
+elseif strcmp(whatComputer,'andrewbuggee')==true
+
+    % -------------------------------------
+    % ------ Folders on my Macbook --------
+    % -------------------------------------
+
+    % ----- Define the MODIS folder name -----
+
+    modisFolder = ['/Users/andrewbuggee/Documents/MATLAB/Matlab-Research/Hyperspectral_Cloud_Retrievals/',...
+        'MODIS_Cloud_Retrieval/MODIS_data/'];
+
+
+elseif strcmp(whatComputer,'curc')==true
+
+
+    % ------------------------------------------------
+    % ------ Folders on the CU Super Computer --------
+    % ------------------------------------------------
+
+    % Define the MODIS folder name
+
+    modisFolder = '/projects/anbu8374/MODIS_data/';
+
+
+end
+
+
+% Loop through three MODIS scenes used in this analysis and store retrieved
+% effective radius and optical thickness
+
+% ----- November 9th at decimal time 0.611 (14:40) -----
+% ----- November 11th at decimal time 0.604 (14:30) -----
+% ----- November 11th at decimal time 0.784 (18:50) -----   
+modisData = {'2008_11_09/', '2008_11_11_1430/', '2008_11_11_1850/'};
+
+% variables to keep
+H_sp = [];
+
+
+% limit to only pixels over ocean with an optical depth of at least 3
+tau_min = 3;
+
+
+for nn = 1:length(modisData)
+
+    [modis,L1B_fileName] = retrieveMODIS_data([modisFolder, modisData{nn}]);
+
+
+    modis_lat = double(modis.geo.lat(:));
+    modis_long = double(modis.geo.long(:));
+    
+    isOcean = land_or_ocean(modis_lat, modis_long, 15, false);
+
+    % Show pixels over ocean
+    figure; 
+    geoscatter(modis_lat(isOcean), modis_long(isOcean), 100, modis.cloud.effRadius17(isOcean), '.');
+    colorbar
+
+
+    
+    % ------------- effective radius -------------------
+    % grab effective radius uncertainty over ocean and convert from percent uncertainty to microns
+    temp_effRad_uncert_17 = modis.cloud.effRadius17(isOcean) .* modis.cloud.effRad_uncert_17(isOcean) .* 0.01;          % microns
+
+    % find values whose uncertainty is less than 0 or nan (applied to non-cloudy
+    % pixels) and with an optical depth of less than 3
+    pix_lessThan0_or_Nan_or_tauLessThan = temp_effRad_uncert_17<0 | isnan(temp_effRad_uncert_17) | modis.cloud.optThickness17(isOcean) < tau_min ...
+                                          | modis.cloud.phase~=1;
+
+    % remove values found above
+    temp_effRad_uncert_17(pix_lessThan0_or_Nan_or_tauLessThan) = [];
+
+    % store the sub-pixel inhomogeneity index for warm cloud pixels with an
+    % optical depth of at least 3, and a positive non-NAN effective radius
+    % uncertainty
+    H_sp = [];
+
+
+    % grab effective radius uncertainty over ocean 
+    temp_effRad_uncert_17_percent = modis.cloud.effRad_uncert_17(isOcean);          % percent of retrieved effective radius
+    
+    % find values whose uncertainty is less than 0 or nan (applied to non-cloudy
+    % pixels) and with an optical depth of less than 3
+    pix_lessThan0_or_Nan_or_tauLessThan = temp_effRad_uncert_17_percent<0 | isnan(temp_effRad_uncert_17_percent) | modis.cloud.optThickness17(isOcean) < tau_min;
+
+    % remove values found above
+    temp_effRad_uncert_17_percent(pix_lessThan0_or_Nan_or_tauLessThan) = [];
+
+    % store remaining values in global variable
+    effRad_uncert_17_percent = [effRad_uncert_17_percent; temp_effRad_uncert_17_percent];
+
+
+
+    
+    % ------------- optical depth -------------------
+    % grab optical depth uncertainty over ocean and convert from percent
+    % uncertainty to opical depth
+    temp_optThickness_uncert_17 = modis.cloud.optThickness17(isOcean) .* modis.cloud.optThickness_uncert_17(isOcean) .* 0.01;          % optical depth
+
+    % find values whose uncertainty is less than 0 or nan (applied to non-cloudy
+    % pixels) and with an optical depth of less than 3
+    pix_lessThan0_or_Nan_or_tauLessThan = temp_optThickness_uncert_17<0 | isnan(temp_optThickness_uncert_17) | modis.cloud.optThickness17(isOcean) < tau_min;
+
+    % remove values found above
+    temp_optThickness_uncert_17(pix_lessThan0_or_Nan_or_tauLessThan) = [];
+
+    % store remaining values in a global variable
+    optThickness_uncert_17 = [optThickness_uncert_17; temp_optThickness_uncert_17];        % microns
+
+
+    % grab optical depth uncertainty over ocean 
+    temp_optThickness_uncert_17_percent = modis.cloud.optThickness_uncert_17(isOcean);           % percent of retrieved optical depth
+    
+    % find values whose uncertainty is less than 0 or nan (applied to non-cloudy
+    % pixels) and with an optical depth of less than 3
+    pix_lessThan0_or_Nan_or_tauLessThan = temp_optThickness_uncert_17_percent<0 | isnan(temp_optThickness_uncert_17_percent) | modis.cloud.optThickness17(isOcean) < tau_min;
+
+    % remove values found above
+    temp_optThickness_uncert_17_percent(pix_lessThan0_or_Nan_or_tauLessThan) = [];
+
+    % store remaining values in global variable
+    optThickness_uncert_17_precent = [optThickness_uncert_17_precent; temp_optThickness_uncert_17_percent];
+    
+
+    
+  
+
+end
+
+
+% create histogram of COD and CER uncertainty for pixels over ocean
+figure; 
+
+subplot(1,2,1)
+histogram(H_sp)
+grid on; grid minor
+ylabel('Counts', 'interpreter', 'latex')
+xlabel('$\delta r_e$ $(\mu m)$', 'interpreter', 'latex')
+set(gca, 'YScale', 'log')
+
+subplot(1,2,2)
+histogram(optThickness_uncert_17)
+grid on; grid minor
+ylabel('Counts', 'interpreter', 'latex')
+xlabel('$\delta \tau_c$', 'interpreter', 'latex')
+set(gca, 'YScale', 'log')
+
+set(gcf, 'Position', [0,0, 1300, 750])
+
+
+figure; 
+
+subplot(1,2,1)
+histogram(effRad_uncert_17_percent)
+grid on; grid minor
+ylabel('Counts', 'interpreter', 'latex')
+xlabel('$\delta r_e$ $(\%)$', 'interpreter', 'latex')
+set(gca, 'YScale', 'log')
+
+subplot(1,2,2)
+histogram(optThickness_uncert_17_precent)
+grid on; grid minor
+ylabel('Counts', 'interpreter', 'latex')
+xlabel('$\delta \tau_c$  (\%)', 'interpreter', 'latex')
+set(gca, 'YScale', 'log')
+
+set(gcf, 'Position', [0,0, 1300, 750])
+
+% print the mean values of uncertainty as a percent for each variable
+disp([newline, 'Average CER uncertainty for pixels over ocean: ', ...
+    num2str(mean(effRad_uncert_17_percent)), '%, with a standard deviation of: ',...
+    num2str(std(effRad_uncert_17_percent)), '%', newline])
+disp([newline, 'Average COD uncertainty for pixels over ocean: ', ...
+    num2str(mean(optThickness_uncert_17_precent)), '%, with a standard deviation of: ',...
+    num2str(std(optThickness_uncert_17_precent)), '%', newline])
+
+% print the mean values of uncertainty for each variable
+disp([newline, 'Average CER uncertainty for pixels over ocean: ', ...
+    num2str(mean(H_sp)), ' microns, with a standard deviation of: ',...
+    num2str(std(H_sp)), ' microns', newline])
+disp([newline, 'Average COD uncertainty for pixels over ocean: ', ...
+    num2str(mean(optThickness_uncert_17)), ', with a standard deviation of: ',...
+    num2str(std(optThickness_uncert_17)), newline])
+
+% Create plot of retireved effective radius versus retrieval uncertainaty
+% and include a one-to-one line
+
+
+
+clear variables
 
