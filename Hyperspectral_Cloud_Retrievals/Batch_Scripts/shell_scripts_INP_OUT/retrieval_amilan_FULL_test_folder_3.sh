a0a88296ca2b4aa9a763c6b9aa4d7b78609f52a1
Andrew John Buggee <84047898+andrewjbuggee@users.noreply.github.com>
2025-08-27 19:11:36 -0600

    new shell scripts for job arrays

--- Hyperspectral_Cloud_Retrievals/Batch_Scripts/shell_scripts_INP_OUT/retrieval_amilan_FULL_test_folder_3.sh
+++ Hyperspectral_Cloud_Retrievals/Batch_Scripts/shell_scripts_INP_OUT/retrieval_amilan_FULL_test_folder_3.sh
@@ -4,14 +4,14 @@
 
 
 #SBATCH --nodes=1
-#SBATCH --time=01:30:00
+#SBATCH --time=02:00:00
 #SBATCH --partition=amilan
 #SBATCH --qos=normal
-#SBATCH --mem=35G
-#SBATCH --ntasks=10
-#SBATCH --cpus-per-task=1
-#SBATCH --job-name=test_retrieval_amilan_10Tasks_FULL_folder_3
-#SBATCH --output=test_retrieval_amilan_10Tasks_FULL_folder_3.out
+#SBATCH --mem=30G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --job-name=test_retrieval_amilan_20Tasks_FULL_folder_3
+#SBATCH --output=test_retrieval_amilan_20Tasks_FULL_folder_3.out
 #SBATCH --mail-user=anbu8374@colorado.edu
 #SBATCH --mail-type=ALL
 
@@ -35,10 +35,10 @@ export PATH=$GSL_BIN:$PATH
 
 cd /projects/anbu8374/
 
-module load matlab
+module load matlab/R2024b
 
 echo "Starting MATLAB job at $(date)"
 
-time matlab -nodesktop -nodisplay -r "addpath(genpath('/projects/anbu8374/Matlab-Research')); addpath(genpath('/scratch/alpine/anbu8374/HySICS/INP_OUT/')); addpath(genpath('/scratch/alpine/anbu8374/Mie_Calculations/')); clear variables; addLibRadTran_paths; [folder_paths, which_computer] = define_folderPaths_for_HySICS(3); print_status_updates = true;print_libRadtran_err = true; test_retrieval_HySICS_no_msgs_orDefinedFolder; exit"
+time matlab -nodesktop -nodisplay -r "addpath(genpath('/projects/anbu8374/Matlab-Research')); addpath(genpath('/scratch/alpine/anbu8374/HySICS/INP_OUT/')); addpath(genpath('/scratch/alpine/anbu8374/Mie_Calculations/')); clear variables; addLibRadTran_paths; [folder_paths, which_computer] = define_folderPaths_for_HySICS(3); print_status_updates = true; print_libRadtran_err = true; test_retrieval_HySICS_no_msgs_orDefinedFolder; exit"
 
 echo "Finished MATLAB job at $(date)"
