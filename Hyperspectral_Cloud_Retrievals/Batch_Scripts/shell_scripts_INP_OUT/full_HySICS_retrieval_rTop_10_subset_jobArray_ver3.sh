550b4f37173a0d8112e8ff7fa0a15f74d32204d5
Andrew John Buggee <anbu8374@colorado.edu>
2025-09-04 14:38:58 -0600

    updates to job array!

--- Hyperspectral_Cloud_Retrievals/Batch_Scripts/shell_scripts_INP_OUT/full_HySICS_retrieval_rTop_10_subset_jobArray_ver3.sh
+++ Hyperspectral_Cloud_Retrievals/Batch_Scripts/shell_scripts_INP_OUT/full_HySICS_retrieval_rTop_10_subset_jobArray_ver3.sh
@@ -55,13 +55,13 @@ module load matlab/R2024b
 # Define the directory containing your input files
 INPUT_DIR="/projects/anbu8374/Matlab-Research/Hyperspectral_Cloud_Retrievals/HySICS/Simulated_spectra/paper2_variableSweep/rTop_10/vza_7_vaz_210_sza_10_saz_91_subset"
 
-RETRIEVED_PROFS_DIR="/projects/anbu8374/Matlab-Research/Hyperspectral_Cloud_Retrievals/HySICS/Droplet_profile_retrievals/paper2_variableSweep/rTop_7/vza_7_vaz_210_sza_10_saz_91_subset/"
+RETRIEVED_PROFS_DIR="/projects/anbu8374/Matlab-Research/Hyperspectral_Cloud_Retrievals/HySICS/Droplet_profile_retrievals/paper2_variableSweep/rTop_10/vza_7_vaz_210_sza_10_saz_91_subset"
 
 # Get list of all files
 mapfile -t ALL_FILES < <(find "${INPUT_DIR}" -maxdepth 1 -name "*.mat" -type f -printf "%f\n" | sort)
 
 # Calculate which files this job should process
-FILES_PER_JOB=4
+FILES_PER_JOB=1
 START_IDX=$(( (SLURM_ARRAY_TASK_ID - 1) * FILES_PER_JOB ))
 END_IDX=$(( START_IDX + FILES_PER_JOB - 1 ))
 
