f571a458eef6dbf39845cfc01dd0edaef10489c3
Andrew John Buggee <84047898+andrewjbuggee@users.noreply.github.com>
2025-09-05 12:02:32 -0600

    minor changes

--- Hyperspectral_Cloud_Retrievals/Batch_Scripts/Paper-2/Droplet_profile_retrievals/compute_dropletProfile_for_HySICS_noACPW_slurm.m
+++ Hyperspectral_Cloud_Retrievals/Batch_Scripts/Paper-2/Droplet_profile_retrievals/compute_dropletProfile_for_HySICS_noACPW_slurm.m
@@ -107,6 +107,6 @@ files{1} = filenames.name;
 
 
 % *** Retrieve r_top, r_bot, tau_c ***
-[tblut_retrieval, GN_inputs, GN_outputs] = run_retrieval_dropletProfile_HySICS_ver3_noACPW_15(files,...
+[tblut_retrieval, GN_inputs, GN_outputs] = run_retrieval_dropletProfile_HySICS_ver3_noACPW_10(files,...
     folder_paths, print_status_updates, print_libRadtran_err);
 
