4a25e229c519a9d09e2506e753ccb9d277b09bff
Andrew John Buggee <anbu8374@colorado.edu>
2025-09-08 13:04:53 -0600

    update to create measurements

--- Hyperspectral_Cloud_Retrievals/Batch_Scripts/shell_scripts_INP_OUT/create_measurements_for_paper2_noOutput_rTop_10_subset_pt1Percent.sh
+++ Hyperspectral_Cloud_Retrievals/Batch_Scripts/shell_scripts_INP_OUT/create_measurements_for_paper2_noOutput_rTop_10_subset_pt1Percent.sh
@@ -39,6 +39,6 @@ module load matlab
 
 echo "Starting MATLAB job at $(date)"
 
-time matlab -nodesktop -nodisplay -r "addpath(genpath('/projects/anbu8374/Matlab-Research')); addpath(genpath('/scratch/alpine/anbu8374/HySICS/INP_OUT/')); addpath(genpath('/scratch/alpine/anbu8374/Mie_Calculations/')); generate_hysics_measurements_noOutput_paper2_rTop_10_pt1Percent; exit"
+time matlab -nodesktop -nodisplay -r "addpath(genpath('/projects/anbu8374/Matlab-Research')); addpath(genpath('/scratch/alpine/anbu8374/HySICS/INP_OUT/')); addpath(genpath('/scratch/alpine/anbu8374/Mie_Calculations/')); clear variables; addLibRadTran_paths; folder_paths = define_folderPaths_for_HySICS(1); generate_hysics_measurements_noOutput_paper2_rTop_10_pt1Percent; exit"
 
 echo "Finished MATLAB job at $(date)"
