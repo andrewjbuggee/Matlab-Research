dfa36e848e1fcdf9f17fec7c7fd6b53cd7a14a64
Andrew John Buggee <84047898+andrewjbuggee@users.noreply.github.com>
2025-09-08 11:25:10 -0600

    minor updates

--- Hyperspectral_Cloud_Retrievals/Batch_Scripts/shell_scripts_INP_OUT/create_measurements_for_paper2_noOutput_rTop_10_subset_5Percent.sh
+++ Hyperspectral_Cloud_Retrievals/Batch_Scripts/shell_scripts_INP_OUT/create_measurements_for_paper2_noOutput_rTop_10_subset_5Percent.sh
@@ -35,10 +35,22 @@ export PATH=$GSL_BIN:$PATH
 
 cd /projects/anbu8374/
 
+
+# Define the directory where the files generated should be saved
+# ----------------------------------------------------------
+# *** MODIFY THIS DIRECTORY BASED ON THE LOCATION OF THE MEASUREMENTS ***
+# *** CANNOT HAVE TRAILING SLASH '/' AT THE END         ***
+INPUT_DIR="/projects/anbu8374/Matlab-Research/Hyperspectral_Cloud_Retrievals/HySICS/Simulated_spectra/paper2_variableSweep/rTop_10/vza_7_vaz_210_sza_10_saz_91_subset/"
+
+
+
 module load matlab
 
-echo "Starting MATLAB job at $(date)"
+# Run MATLAB once with all files for this job
+echo " "
+echo "Starting MATLAB at $(date)"
 
-time matlab -nodesktop -nodisplay -r "addpath(genpath('/projects/anbu8374/Matlab-Research')); addpath(genpath('/scratch/alpine/anbu8374/HySICS/INP_OUT/')); addpath(genpath('/scratch/alpine/anbu8374/Mie_Calculations/')); generate_hysics_measurements_noOutput_paper2_rTop_10_5Percent; exit"
+time matlab -nodesktop -nodisplay -r "addpath(genpath('/projects/anbu8374/Matlab-Research')); addpath(genpath('/scratch/alpine/anbu8374/HySICS/INP_OUT/')); addpath(genpath('/scratch/alpine/anbu8374/Mie_Calculations/')); clear variables; addLibRadTran_paths; folder_paths = define_folderPaths_for_HySICS('${SLURM_ARRAY_JOB_ID}'); folder_paths.HySICS_simulated_spectra = '${INPUT_DIR}/'; print_libRadtran_err = true; generate_hysics_measurements_noOutput_paper2_rTop_10_5Percent; exit"
 
-echo "Finished MATLAB job at $(date)"
+echo " "
+echo "Finished MATLAB job array task ${SLURM_ARRAY_TASK_ID} at $(date)"
