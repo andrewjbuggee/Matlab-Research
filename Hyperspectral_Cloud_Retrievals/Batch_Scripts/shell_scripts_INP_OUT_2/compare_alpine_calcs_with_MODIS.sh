8d463c5975b9e1c4f999e2a275b3bd668b8b1d3c
Andrew John Buggee <84047898+andrewjbuggee@users.noreply.github.com>
2025-09-13 16:58:05 -0600

    Do my CURC calcs fare well against MODIS observations?

---
+++ Hyperspectral_Cloud_Retrievals/Batch_Scripts/shell_scripts_INP_OUT_2/compare_alpine_calcs_with_MODIS.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=30G
+#SBATCH --time=1:00:00     # Longer time for multiple files
+#SBATCH --partition=amilan
+#SBATCH --qos=normal
+#SBATCH --job-name=compare_alpine_calcs_with_MODIS
+#SBATCH --output=compare_alpine_calcs_with_MODIS.out
+#SBATCH --error=compare_alpine_calcs_with_MODIS.err
+#SBATCH --mail-user=anbu8374@colorado.edu
+#SBATCH --mail-type=ALL
+
+
+ml purge
+ml gcc/11.2.0
+ml netcdf/4.8.1
+ml perl/5.36.0
+ml texlive/2021
+
+
+export PATH=/projects/$USER/software/libRadtran-2.0.5/:$PATH
+export PATH=/projects/$USER/software/libRadtran-2.0.5/data/:$PATH
+export PATH=/projects/$USER/software/libRadtran-2.0.5/bin/:$PATH
+export GSL_BIN=/projects/$USER/software/gsl-2.6/bin
+export GSL_LIB=/projects/$USER/software/gsl-2.6/lib
+export GSL_INC=/projects/$USER/software/gsl-2.6/include
+export LD_LIBRARY_PATH=$GSL_LIB:$LD_LIBRARY_PATH
+export INSTALL_DIR=/projects/$USER/software/libRadtran-2.0.5
+export PATH=$GSL_BIN:$PATH
+
+cd /projects/anbu8374/
+
+module load matlab/R2024b
+
+echo "Starting MATLAB job at $(date)"
+
+time matlab -nodesktop -nodisplay -r "addpath(genpath('/projects/anbu8374/Matlab-Research')); addpath(genpath('/scratch/alpine/anbu8374/HySICS/INP_OUT/')); addpath(genpath('/scratch/alpine/anbu8374/Mie_Calculations/')); modis_reflectance_versus_modeled_ensemble_statistics; exit"
+
+echo "Finished MATLAB job at $(date)"
+
