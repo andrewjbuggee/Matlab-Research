2bfb9e7a5658f4a64455086c2d6da97ec1d2451d
Andrew John Buggee <84047898+andrewjbuggee@users.noreply.github.com>
2025-10-20 14:48:13 -0600

    updates to fitting distribution to vocalsRex

--- Hyperspectral_Cloud_Retrievals/VOCALS_REx/find_bestFitDist_dropDist.m
+++ Hyperspectral_Cloud_Retrievals/VOCALS_REx/find_bestFitDist_dropDist.m
@@ -27,35 +27,90 @@
 
 % By Andrew John Buggee
 
-%% 
+%%
 
 
-function [fits, goodness] = find_bestFitDist_dropDist(droplet_dist)
+function [h_norm, p_norm, h_logNorm, p_logNorm, h_gamma, p_gamma] = find_bestFitDist_dropDist(droplet_dist,...
+    binEdges, binCenters, significance_lvl)
 
 
 % length of the profile - number of distribution measurements
 data_length = size(droplet_dist,2);
 
+% set up hypothesis testing vectors
+h_norm = zeros(1, data_length);
+h_logNorm = zeros(1, data_length);
+h_gamma = zeros(1, data_length);
+
+p_norm = zeros(1, data_length);
+p_logNorm = zeros(1, data_length);
+p_gamma = zeros(1, data_length);
+
+% Values of 0 cause the lognoraml fit to fail. Set these to some small
+% value greater than 0
+droplet_dist(droplet_dist==0) = 1e-10;
+
 for zz = 1:data_length
 
+    % The chi-squared method checks the differences between the observed
+    % counts in each bin with the expected counts in each bin. The p-value
+    % is the probability of getting the chi-squared value we got by chance.
+    % Then, we check to see if the p-value is less than or equal to the
+    % significance value. The significance value is the probability that we
+    % reject the fit, but it's actually a good one:
+
+        % If p-value < α: Reject the null hypothesis (the distribution is NOT a good fit)
+        % If p-value ≥ α: Fail to reject (the distribution is a reasonable fit)
 
 
-    %% Fit a normal distribution and compute the chi^2 goodness of fit 
+    %% Fit a normal distribution and compute the chi^2 goodness of fit
+
+
+
+    % gauss_fit = fitdist(droplet_dist(:,zz), 'normal');
+    %
+    % [h, p, st] = chi2gof(droplet_dist(:,zz),'CDF',gauss_fit);
+
 
     gauss_fit = fitdist(droplet_dist(:,zz), 'normal');
 
-    
+    expected_counts_gauss = sum(droplet_dist(:,zz)) * pdf(gauss_fit, binCenters);
+
+    % There are two free parameters that define the normal distribution
+    [h_norm(zz), p_norm(zz)] = chi2gof(binCenters,'Ctrs',binCenters,'Frequency', droplet_dist(:,zz), ...
+        'Expected',expected_counts_gauss, 'Alpha', significance_lvl,...
+        'NParams',2);
+
     %% Fit a lognormal distribution and compute the R^2 value
 
+
+    logNormal_fit = fitdist(droplet_dist(:,zz), 'Lognormal');
+
+    expected_counts_logNorm = sum(droplet_dist(:,zz)) * pdf(logNormal_fit, binCenters);
+
+    % There are two free parameters that define the lognormal distribution
+    [h_logNorm(zz), p_logNorm(zz)] = chi2gof(binCenters,'Ctrs',binCenters,'Frequency', droplet_dist(:,zz), ...
+        'Expected',expected_counts_logNorm, 'Alpha', significance_lvl,...
+        'NParams',2);
 
 
 
     %% Fit a gamma distribution and compute the R^2 value
 
+    gamma_fit = fitdist(droplet_dist(:,zz), 'Gamma');
+
+    expected_counts_gamma = sum(droplet_dist(:,zz)) * pdf(gamma_fit, binCenters);
+
+    % There are two free parameters that define the Gamma distribution
+    [h_gamma(zz), p_gamma(zz)] = chi2gof(binCenters,'Ctrs',binCenters,'Frequency', droplet_dist(:,zz), ...
+        'Expected',expected_counts_gamma, 'Alpha', significance_lvl,...
+        'NParams',2);
 
 
 
 end
+
+
 
 
 
