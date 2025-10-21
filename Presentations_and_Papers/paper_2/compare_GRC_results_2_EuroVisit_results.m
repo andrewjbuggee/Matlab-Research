6d0669b460e305245f38007852e35fb3cf9f3559
Andrew John Buggee <84047898+andrewjbuggee@users.noreply.github.com>
2025-10-21 16:19:34 -0600

    minor plot updates

--- Presentations_and_Papers/paper_2/compare_GRC_results_2_EuroVisit_results.m
+++ Presentations_and_Papers/paper_2/compare_GRC_results_2_EuroVisit_results.m
@@ -22,7 +22,7 @@ if strcmp(which_computer,'anbu8374')==true
     folder_paths.HySICS_retrievals = ['/Users/anbu8374/Documents/MATLAB/Matlab-Research/Hyperspectral_Cloud_Retrievals/',...
         'HySICS/Droplet_profile_retrievals/'];
 
-    folder_paths.drive = 
+    folder_paths.drive = '/Users/anbu8374/MATLAB-Drive/HySICS/Droplet_profile_retrievals/testGRC_results/';
 
 
 elseif strcmp(which_computer,'andrewbuggee')==true
@@ -36,6 +36,7 @@ elseif strcmp(which_computer,'andrewbuggee')==true
         'Hyperspectral_Cloud_Retrievals/HySICS/Droplet_profile_retrievals/'];
 
 
+    folder_paths.drive = '/Users/andrewbuggee/MATLAB-Drive/HySICS/Droplet_profile_retrievals/testGRC_results/';
 
 
 elseif strcmp(which_computer,'curc')==true
@@ -61,11 +62,29 @@ filenames = {'dropletRetrieval_HySICS_35bands_withNoise_10mm-totalCWV_sim-ran-on
 %     'dropletRetrieval_HySICS_35bands_withNoise_20mm-totalCWV_sim-ran-on-13-Jul-2025_rev1.mat',...
 %     'dropletRetrieval_HySICS_66bands_withNoise_cwvRetrieval_sim-ran-on-12-Jul-2025_rev1.mat'};
 
+% Grab filenames in drive
+filenames = dir(folder_paths.drive);
+idx_2delete = [];
+for nn = 1:length(filenames)
+
+    if strcmp(filenames(nn).name(1), 'd')~=true
+
+        idx_2delete = [idx_2delete, nn];
+
+    end
+
+end
+
+% delete rows that don't have retrieval filenames
+filenames(idx_2delete) = [];
+
+
+
 
 % Step through each file
 
 % define the colors for each curve plotted
-C = mySavedColors(61:(61+length(filenames)+1), 'fixed');
+C = mySavedColors(61:(61+length(filenames) +1), 'fixed');
 
 lgnd_str = cell(1, length(filenames) + 2);
 
@@ -178,8 +197,207 @@ end
 
 
 % --- *** Overlay new retrieval results *** ---
+% define a new set of colors
+C2 = mySavedColors(61+length(filenames) +1 : 61+length(filenames) +1 + length(filenames)+1, 'fixed');
+lgnd_str2 = cell(1, length(filenames));
+
+% Load a data set
+for nn = 1:length(filenames)
+
+    ds = load([filenames(nn).folder, '/', filenames(nn).name]);
+
+    errorbar(ds.GN_outputs.retrieval(2,end), ds.GN_outputs.retrieval(1,end), sqrt(ds.GN_outputs.posterior_cov(1,1))/2,...
+        sqrt(ds.GN_outputs.posterior_cov(1,1))/2, sqrt(ds.GN_outputs.posterior_cov(2,2))/2,...
+        sqrt(ds.GN_outputs.posterior_cov(2,2))/2, 'MarkerFaceColor', C2(nn+1,:),...
+        'MarkerEdgeColor', C2(nn+1,:), 'Linewidth', 4, 'Marker', '.', 'MarkerSize', 40,...
+        'Color', C2(nn+1,:))
+
+    hold on
 
 
+    % what was the retrieved above cloud column water vapor path above
+    % cloud?
+    retrieved_CWV = ds.GN_outputs.retrieval(end, end);        % kg/m^2 (mm)
+
+    lgnd_str2{nn} = ['*new retrieval* - ', num2str(numel(ds.GN_inputs.bands2run)), ' bands - ',...
+        num2str(round(ds.GN_inputs.measurement.uncertainty(1)*100,1)), '$\%$ uncert - retrieved $acpw$ = ',...
+        num2str(round(retrieved_CWV, 2)), ' $mm$'];
+
+end
+
+
+
+% ----------------------------------------------
+
+
+% Create a Legend with only the two black curves
+legend([lgnd_str, lgnd_str2], 'Interpreter','latex', 'Location','northwest', 'FontSize', 20)
+
+grid on; grid minor
+ylabel('$r_{top}$ ($\mu m$)', 'Interpreter','latex', 'FontSize',30)
+xlabel('$r_{bot}$ ($\mu m$)', 'Interpreter','latex', 'FontSize',30)
+
+set(gcf,'Position',[0 0 950 750])
+
+
+%% Compare three retrievals with different uncertinaty
+
+
+%% Plot the GRC results and overlay the three retrievals with measurement uncertainty of 0%, 0.3% and 1% using the EuroVisit algorithm
+
+% the one file with retrieved column water vapor
+
+% *** Using HySICS data ***
+
+clear variables
+
+
+% Determine which computer you're using
+which_computer = whatComputer();
+
+% Find the folder where the mie calculations are stored
+% find the folder where the water cloud files are stored.
+if strcmp(which_computer,'anbu8374')==true
+
+    % -----------------------------------------
+    % ------ Folders on my Mac Desktop --------
+    % -----------------------------------------
+
+    % ---- Define where the retrievals are stored ---
+    folder_paths.drive = '/Users/anbu8374/MATLAB-Drive/HySICS/Droplet_profile_retrievals/testGRC_results/';
+
+
+elseif strcmp(which_computer,'andrewbuggee')==true
+
+    % -------------------------------------
+    % ------ Folders on my Macbook --------
+    % -------------------------------------
+
+    % ---- Define where the retrievals are stored ---
+    folder_paths.drive = '/Users/andrewbuggee/MATLAB-Drive/HySICS/Droplet_profile_retrievals/testGRC_results/';
+
+
+elseif strcmp(which_computer,'curc')==true
+
+
+    % ------------------------------------------------
+    % ------ Folders on the CU Super Computer --------
+    % ------------------------------------------------
+
+end
+
+
+
+% Grab filenames in drive
+filenames = dir(folder_paths.drive);
+idx_2delete = [];
+for nn = 1:length(filenames)
+
+    if strcmp(filenames(nn).name(1), 'd')~=true
+
+        idx_2delete = [idx_2delete, nn];
+
+    end
+
+end
+
+% delete rows that don't have retrieval filenames
+filenames(idx_2delete) = [];
+
+
+
+
+% Step through each file
+
+% define the colors for each curve plotted
+C = mySavedColors(61:(61+length(filenames) +1), 'fixed');
+
+lgnd_str = cell(1, length(filenames) + 2);
+
+
+figure;
+
+
+for nn = 1:length(filenames)
+
+
+    % Load a data set
+    ds = load([filenames(nn).folder, '/', filenames(nn).name]);
+
+    if nn==1
+
+
+        % first, plot the simulated profile values as two lines
+        xline(ds.GN_inputs.RT.r_bot, ':', ['Simulated $r_{bot}$'],...
+            'Fontsize',20, 'Interpreter','latex','LineWidth',2,'Color', [147/255, 150/255, 151/255], 'LabelHorizontalAlignment','left',...
+            'LabelVerticalAlignment','bottom');
+
+        hold on
+
+        yline(ds.GN_inputs.RT.r_top, ':', ['Simulated $r_{top}$'],...
+            'Fontsize',20, 'Interpreter','latex','LineWidth',2,'Color', [147/255, 150/255, 151/255], 'LabelHorizontalAlignment','right',...
+            'LabelVerticalAlignment','top');
+        hold on
+
+
+        % what was the assumed above cloud column water vapor path?
+        simulated_CWV = ds.GN_inputs.measurement.actpw; % kg/m^2
+
+        title(['Simulated profile - $acpw$ = ',num2str(round(simulated_CWV, 2)), ' $mm$'],...
+            'Fontsize', 25, 'Interpreter', 'latex');
+
+        % Skip the first two legend entries
+        lgnd_str{1} = '';
+        lgnd_str{2} = '';
+
+    end
+
+
+
+    % plot the retrieved droplet profile
+
+
+    % give a different marker type for the retrieval using 66 bands
+    % that also retrieved above cloud column water vapor
+
+
+    errorbar(ds.GN_outputs.retrieval(2,end), ds.GN_outputs.retrieval(1,end), sqrt(ds.GN_outputs.posterior_cov(1,1))/2,...
+        sqrt(ds.GN_outputs.posterior_cov(1,1))/2, sqrt(ds.GN_outputs.posterior_cov(2,2))/2,...
+        sqrt(ds.GN_outputs.posterior_cov(2,2))/2, 'MarkerFaceColor', C(nn+1,:),...
+        'MarkerEdgeColor', C(nn+1,:), 'Linewidth', 4, 'Marker', '.', 'MarkerSize', 40,...
+        'Color', C(nn+1,:))
+
+    hold on
+
+
+
+
+
+
+
+
+    % create the string for the retrieval using CWV
+
+    % what was the retrieved above cloud column water vapor path above
+    % cloud?
+    retrieved_CWV = ds.GN_outputs.retrieval(end, end);        % kg/m^2 (mm)
+
+    lgnd_str{nn+2} = ['*new retrieval* - ', num2str(numel(ds.GN_inputs.bands2run)), ' bands - ',...
+        num2str(round(ds.GN_inputs.measurement.uncertainty(1)*100,1)), '$\%$ uncert - retrieved $acpw$ = ',...
+        num2str(round(retrieved_CWV, 2)), ' $mm$'];
+
+
+
+end
+
+
+
+
+
+
+
+
+% ----------------------------------------------
 
 
 % Create a Legend with only the two black curves
