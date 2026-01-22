95e379241652422e1b65d1d24ee8db135477ce54
Andrew John Buggee <84047898+andrewjbuggee@users.noreply.github.com>
2026-01-21 19:17:37 -0700

    run precomputed mie tables on the super computer!

--- Radiative_Transfer_Physics/mieTables_gamma/precomputeMieTables_slurm.m
+++ Radiative_Transfer_Physics/mieTables_gamma/precomputeMieTables_slurm.m
@@ -6,17 +6,60 @@
 
 function precomputeMieTables_slurm(folder_path)
 
+clear inputs
+
+
+which_computer = whatComputer;
+
+%% Start parallel pool
+
+% *** Start parallel pool ***
+% Is parpool running?
+p = gcp('nocreate');
+if isempty(p)==true
+
+    % first read the local number of workers avilabile.
+    p = parcluster('local');
+    % start the cluster with the number of workers available
+    if p.NumWorkers>64
+        % Likely the amilan128c partition with 2.1 GB per core
+        % Leave some cores for overhead
+        parpool(p.NumWorkers - 8);
+
+    elseif p.NumWorkers<=64 && p.NumWorkers>10
+
+        % Leave a core for overhead
+        parpool(p.NumWorkers -1);
+
+    elseif p.NumWorkers<=10
+
+        % Leave a core for overhead
+        parpool(p.NumWorkers -1);
+
+
+    end
+
+end
+
+%% Find all INP files and run them in a parallel for loop!
+
 
 % Find all .INP files within the input defined folder path
 inpFiles = dir(fullfile(folder_path, '*.INP'));
 
 % step through each .INP file in a parallel for loop and run the .INP file
 % using runMie
+outputFileName = cell(size(inpFiles));
 
 parfor nn = 1:length(inpFiles)
 
+    % Can i set the output name? I don't think it's used when output_user
+    % netcdf is enabled
+    outputFileName{nn} = ['OUTPUT_', inpFiles(nn).name(1:end-4)];
+
+
     % run the mie file
-    runMIE(mie_folder_path,input_filename,output_filename, computer_name);
+    runMIE(folder_path, inpFiles(nn).name, outputFileName{nn}, which_computer);
 
 end
 
