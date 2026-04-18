7809b2738da323f3ac8fcad6cba7ba5ebd3cc1a5
Andrew John Buggee <84047898+andrewjbuggee@users.noreply.github.com>
2026-04-18 13:13:28 -0600

    maximum of 16 CPUS

--- Hyperspectral_Cloud_Retrievals/Batch_Scripts/bash_scripts/neural_network/training_data/10_April_2026_OR/sza41_TEST.sh
+++ Hyperspectral_Cloud_Retrievals/Batch_Scripts/bash_scripts/neural_network/training_data/10_April_2026_OR/sza41_TEST.sh
@@ -13,7 +13,7 @@
 #SBATCH --qos=testing
 #SBATCH --mem=85G        # Should be closer to 80% efficiency based on previous runs, but giving some buffer for variability
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=40
+#SBATCH --cpus-per-task=16
 #SBATCH --job-name=create_meas_pt3_percent_OR_insitu_ERA5_trainingData_sza41
 #SBATCH --output=create_meas_pt3_percent_OR_insitu_ERA5_trainingData_sza41_%A_%a.out
 #SBATCH --error=create_meas_pt3_percent_OR_insitu_ERA5_trainingData_sza41_%A_%a.err
