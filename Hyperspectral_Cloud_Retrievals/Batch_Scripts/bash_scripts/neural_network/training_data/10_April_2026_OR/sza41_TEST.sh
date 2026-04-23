2792fb53f7d27c25331d86d093c9cf98ecc75f81
Andrew Buggee <anbu8374@viz2.rc.int.colorado.edu>
2026-04-23 12:00:05 -0600

    update to the correct QOS

--- Hyperspectral_Cloud_Retrievals/Batch_Scripts/bash_scripts/neural_network/training_data/10_April_2026_OR/sza41_TEST.sh
+++ Hyperspectral_Cloud_Retrievals/Batch_Scripts/bash_scripts/neural_network/training_data/10_April_2026_OR/sza41_TEST.sh
@@ -10,7 +10,7 @@
 #SBATCH --nodes=1
 #SBATCH --time=10:59:00   # Request 10 hours and 59 minutes for longer computation
 #SBATCH --partition=amilan
-#SBATCH --qos=testing
+#SBATCH --qos=normal
 #SBATCH --mem=85G        # Should be closer to 80% efficiency based on previous runs, but giving some buffer for variability
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=40
