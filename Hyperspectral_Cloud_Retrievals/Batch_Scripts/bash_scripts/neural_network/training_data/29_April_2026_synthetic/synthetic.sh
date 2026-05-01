2a17c4644e8d11418ae484bd8053ec1133fef781
Andrew Buggee <anbu8374@viz2.rc.int.colorado.edu>
2026-05-01 09:18:53 -0600

    reduced wall time for shell script

--- Hyperspectral_Cloud_Retrievals/Batch_Scripts/bash_scripts/neural_network/training_data/29_April_2026_synthetic/synthetic.sh
+++ Hyperspectral_Cloud_Retrievals/Batch_Scripts/bash_scripts/neural_network/training_data/29_April_2026_synthetic/synthetic.sh
@@ -9,7 +9,7 @@
 # ----------------------------------------------------------
 #SBATCH --account=ucb762_asc1
 #SBATCH --nodes=1
-#SBATCH --time=02:00:00
+#SBATCH --time=01:00:00
 #SBATCH --partition=amilan
 #SBATCH --qos=normal
 #SBATCH --mem=82G            
